d2e1cc273a434520cdd7d75e05294c818fab3258
Probe001 <97031805+Probe001@users.noreply.github.com>
2023-03-29 15:24:47 +0900

    [Silver II] Title: 외판원 순회 2, Time: 376 ms, Memory: 13772 KB -BaekjoonHub

---
+++ 백준/Silver/10971. 외판원 순회 2/외판원 순회 2.java
@@ -0,0 +1,67 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+import java.util.StringTokenizer;
+
+public class Main {
+	static int N;
+	static int[][] board;
+	static boolean[] vis;
+	static long res;
+	static int[] src;
+	
+	static void dfs(int k, long tmp, int now, int start) {
+		// 인덱스, 경로비용, 현재노드, 시작노드
+		if(k==N) {
+			if(board[now][start] == 0) return;
+			res = Math.min(res, tmp + board[now][start]);
+			src[k] = start;
+			long a = 0;
+			for(int i=0; i<N; i++) {
+				a += board[src[i]][src[i+1]];
+			}
+//			System.out.println(Arrays.toString(src)+" "+res+" "+(tmp + board[now][start])+" "+a);
+			return;
+		}
+		
+		for(int i=0; i<N; i++) { // 다음 목적지 설정
+			if(vis[i]) continue; // 방문한 곳은 지나쳐
+			if(board[now][i] == 0) continue; // 현재 노드에서 다음노드로 가는 길 없을 때
+			vis[i] = true; // 방문처리
+			long t = tmp + board[now][i]; // 현재 노드에서 다음노드 가는 비용 추가
+//			if(t > res) continue; // 이미 최소비용보다 많으면 안가도 됨
+			src[k] = i;
+			dfs(k+1, t, i, start); // 다음 노드로 넘어감
+			vis[i] = false;
+		}
+	}
+
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		
+		N = Integer.parseInt(br.readLine());
+		board = new int[N][N];
+		for(int x=0; x<N; x++)
+		{
+			StringTokenizer st = new StringTokenizer(br.readLine());
+			for(int y=0; y<N; y++) {
+				board[x][y] = Integer.parseInt(st.nextToken());
+			}
+		}
+		vis = new boolean[N];
+		res = Long.MAX_VALUE;
+		src = new int[N+1];
+		for(int st = 0; st<N; st++) {
+			src[0] = st;
+			vis[st] = true;
+			dfs(1, 0, st, st);
+			// 시작점은 정해졌음.
+			vis[st] = false;
+		}
+		System.out.println(res);
+		
+		br.close();
+	}
+
+}
